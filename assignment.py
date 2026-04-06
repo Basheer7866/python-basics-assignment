b540eafd7cc75f48457c5e63af61ba11adec0ea3
Mohammed Basheer <mdbas786@gmail.com>
2026-04-06 16:17:20 +0530

    Intial commit of all files

---
+++ assignment.py
@@ -0,0 +1,39 @@
+name = "Basheer"
+
+age = 25
+
+height = 1.75
+
+is_student = True
+
+
+
+print(name, age, height, is_student)
+
+
+
+# Task 2: String Formatting
+
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+
+
+# Task 3: Arithmetic Operations
+
+age_in_months = age * 12
+
+age_in_days = age * 365
+
+age_remainder = age % 7
+
+age_squared = age ** 2
+
+
+
+print("Age in months:", age_in_months)
+
+print("Age in days:", age_in_days)
+
+print("Remainder when age ÷ 7:", age_remainder)
+
+print("Age squared:", age_squared)
